e58e8b022797cc3d4e1f326c6a3df8079d486fe2
Marcin Grabowiecki <mgr@simple.com.pl>
2013-11-04 16:19:55 +0100

    hibernate init

--- JavaDB/jdbc/src/main/java/com/javahive/javadb/jdbc/ConnectionData.java
+++
@@ -1,8 +0,0 @@
-package com.javahive.javadb.jdbc;
-
-public interface ConnectionData {
-    final static String DB_DRIVER = "org.h2.Driver";
-    final static String DB_URL = "jdbc:h2:testJavaHive";
-    final static String DB_USER = "sa";
-    final static String DB_PASS = "sa";
-}
